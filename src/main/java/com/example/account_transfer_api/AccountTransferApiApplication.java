08d58be07c4fa6856118f21b0991a8d54542cff5
ac <acnacor@gmail.com>
2025-08-12 18:45:35 +1000

    init

---
+++ src/main/java/com/example/account_transfer_api/AccountTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.account_transfer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTransferApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/account_transfer_api/AccountTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.account_transfer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountTransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
